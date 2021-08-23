8d6eaa7932326229686e10f1821cad70934c7970
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2021-08-22 20:43:01 -0600

    added script to calculate total

--- index.js
+++ index.js
@@ -1,8 +1,72 @@
 $(document).ready( () => {
     const bill = $('#bill');
+    const numPeople = $('#num-people');
     bill.css('width', '2rem');
+    let billAmount;
     bill.on('input', ({target}) => {
         const inputLength = target.value.length;
         bill.css("width", `${inputLength + 1}rem`);
     });
+    numPeople.css('width', '2rem');
+    numPeople.on('input', ({target}) => {
+        const inputLength = target.value.length;
+        numPeople.css("width", `${inputLength + 1}rem`);
+    });
+});
+
+let tipPercentage = .15;
+//changes button colors
+const buttons = document.querySelectorAll('.tip-button');
+const custom = document.querySelector('#custom');
+buttons.forEach( (button) => {
+    button.addEventListener("click", ({target}) => {
+        buttons.forEach( x => {
+            x.classList.remove("selected");
+        });
+        custom.value = '';
+        tipPercentage = button.dataset.percentage;
+        target.classList.add("selected");
+        const customSpan = document.querySelector("#tip-percentages span");
+        customSpan.classList.remove("selected");
+        getInfo();
+    });
+});
+custom.addEventListener("input", () => {
+    tipPercentage = custom.value * .01;
+    buttons.forEach( x => {
+        x.classList.remove("selected");
+    });
+    const customSpan = document.querySelector("#tip-percentages span");
+    customSpan.classList.add("selected");
+    getInfo();
+});
+const bill = document.querySelector('#bill');
+const people = document.querySelector('#num-people');
+bill.addEventListener('input', getInfo);
+people.addEventListener('input', getInfo);
+
+function getInfo() {
+    const billAmount = document.querySelector('#bill').value;
+    const numPeople = document.querySelector('#num-people').value;
+    let total = null;
+    if (billAmount !== '' && numPeople !== '') {
+        total = (parseInt(billAmount) + parseInt(billAmount * tipPercentage)) / parseInt(numPeople);
+        const totalResult = document.querySelector('#total');
+        totalResult.innerText = `$${Number(total).toFixed(2)}`;
+    }
+    if (total !== null && numPeople !== '') {
+        const tipAmountResult = document.querySelector('#tip-amount');
+        const tipAmount = total / parseInt(numPeople);
+        tipAmountResult.innerText = `$${Number(tipAmount).toFixed(2)}`;
+    }
+}
+
+const reset = document.querySelector('.reset');
+reset.addEventListener("click", () => {
+    bill.value = '';
+    people.value = '';
+    const tipAmountResult = document.querySelector('#tip-amount');
+    const totalResult = document.querySelector('#total');
+    tipAmountResult.innerText = '';
+    totalResult.innerText = '';
 });
